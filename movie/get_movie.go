f990ab9afd5de3f6bcdc2e28a771a19651b7070f
ryuta06012 <ryuta.06012@gmail.com>
2022-09-24 02:28:16 +0900

    test

--- movie/get_movie.go
+++ movie/get_movie.go
@@ -2,9 +2,11 @@ package movie
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
@@ -14,11 +16,14 @@ import (
 func GetMovie() error {
 	sess := createSession()
 
-	bucketName := "movie"
-	objectKey := "test1.mp4"
-	newFileName := "./sample.mp4"
-	f, err := os.Create(newFileName)
+	bucketName := "dev-musemo-movie-bucket"
+	objectKey := "movie/te.mp4"
+	/* var name = flag.String("name", "John", "User name.")
+	flag.Parse() */
+	filename := "sample.mp4"
+	f, err := os.Create(filename)
 	if err != nil {
+		log.Fatal(err.Error())
 		return err
 	}
 	defer f.Close()
@@ -28,14 +33,16 @@ func GetMovie() error {
 		Key:    aws.String(objectKey),
 	})
 	if err != nil {
+		log.Fatal(err.Error())
 		return err
 	}
-	uploadVideo(newFileName)
+	uploadVideo(filename)
+	println("success!\n")
 	return nil
 }
 
 func uploadVideo(url string) error {
-	client := vimeo.NewClient(nil, "264de4b79b29af50f49579bb8d12283c")
+	client := vimeo.NewClient(nil, os.Getenv("PREMIUM_ACCESS_TOKEN"))
 	uploadRes, _, err := client.Videos.UploadVideo(url)
 	if err != nil {
 		return err
@@ -47,9 +54,11 @@ func uploadVideo(url string) error {
 func createSession() *session.Session {
 	// 特に設定しなくても環境変数にセットしたクレデンシャル情報を利用して接続してくれる
 	cfg := aws.Config{
-		Region:           aws.String("ap-northeast-1"),
-		Endpoint:         aws.String("http://minio:9000"), // コンテナ内からアクセスする場合はホストをサービス名で指定
-		S3ForcePathStyle: aws.Bool(true),                  // ローカルで動かす場合は必須
+		Credentials: credentials.NewStaticCredentials(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), ""),
+		Region:      aws.String("ap-northeast-1"),
+		//s3://dev-musemo-movie-bucket/movie/
+		//Endpoint:         aws.String("http://minio:9000"), // コンテナ内からアクセスする場合はホストをサービス名で指定
+		//S3ForcePathStyle: aws.Bool(true), // ローカルで動かす場合は必須
 	}
 	return session.Must(session.NewSession(&cfg))
 }
